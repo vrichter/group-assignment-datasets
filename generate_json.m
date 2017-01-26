cc2af0f83002b33f2d40910f44b2c4566727a4be
Viktor Richter <vrichter@posteo.de>
2017-01-26 14:50:12 +0100

    Fix generate-json needed prepared folder structure.

--- generate_json.m
+++ generate_json.m
@@ -1,25 +1,28 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Copyright 2017 Viktor Richter                                                   %
-%                                                                                 %
-% Permission is hereby granted, free of charge, to any person obtaining a copy of %
-% this software and associated documentation files (the "Software"), to deal in   %
-% the Software without restriction, including without limitation the rights to    %
-% use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies   %
-% of the Software, and to permit persons to whom the Software is furnished to do  %
-% so, subject to the following conditions:                                        %
-%                                                                                 %
-% The above copyright notice and this permission notice shall be included in all  %
-% copies or substantial portions of the Software.                                 %
-%                                                                                 %
-% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR      %
-% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,        %
-% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE     %
-% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER          %
-% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,   %
-% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE   %
-% SOFTWARE.                                                                       %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                        %
+% Copyright 2017 Viktor Richter                                          %
+%                                                                        %
+% Permission is hereby granted, free of charge, to any person obtaining  %
+% a copy of this software and associated documentation files (the        %
+% "Software"), to deal in the Software without restriction, including    %
+% without limitation the rights to use, copy, modify, merge, publish,    %
+% distribute, sublicense, and/or sell copies of the Software, and to     %
+% permit persons to whom the Software is furnished to do so, subject to  %
+% the following conditions:                                              %
+%                                                                        %
+% The above copyright notice and this permission notice shall be         %
+% included in all copies or substantial portions of the Software.        %
+%                                                                        %
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        %
+% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     %
+% MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND                  %
+% NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS    %
+% BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN     %
+% ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN      %
+% CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE       %
+% SOFTWARE.                                                              %
+%                                                                        %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear all,
 close all,
@@ -45,6 +48,22 @@ files = [ 'features',
           'settings_gc',
           'settings'
         ]';
+
+fprintf('Prepare folder structure.')
+mkdir(strcat(pwd,'/data/json'));
+mkdir(strcat(pwd,'/data/json/CoffeeBreak'));
+mkdir(strcat(pwd,'/data/json/CoffeeBreak/Seq1'));
+mkdir(strcat(pwd,'/data/json/CoffeeBreak/Seq2'));
+mkdir(strcat(pwd,'/data/json/Poster'));
+mkdir(strcat(pwd,'/data/json/CocktailParty'));
+mkdir(strcat(pwd,'/data/json/Synth'));
+mkdir(strcat(pwd,'/data/json/GDet'));
+mkdir(strcat(pwd,'/data/json/GDet/S1'));
+mkdir(strcat(pwd,'/data/json/GDet/S2'));
+mkdir(strcat(pwd,'/data/json/GDet/S3'));
+mkdir(strcat(pwd,'/data/json/GDet/S5'));
+mkdir(strcat(pwd,'/data/json/GDet/S6'));
+
 
 addpath(strcat(pwd,'/jsonlab'))
 
